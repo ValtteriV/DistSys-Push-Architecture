dad183f0d914f303432d84b3d3091b85429ac5ac
Valtteri Vistiaho <valtteri.vistiaho@gmail.com>
2018-12-11 17:59:35 +0200

    Link to Documentation

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from pyfcm import FCMNotification
 
 if __name__ == '__main__':
-    push_service = FCMNotification(api_key="null") #api_key from firebase project
+    push_service = FCMNotification(api_key="AAAA8un-MkA:APA91bFJnBY7q37ERY3cq4F5QULtsV_-YuWLG1e_CxhSfRKzeusdlx-PW73cDdHCzNign3YZvdFv0yeg0O6UikwdVCgo1cmOamrJRqXWHBG19WPJfNS3dXg4fRyGQMa66c5fF3MYOoqG") #api_key from firebase project
     registration_ids = [None] #The id of the client to receive messages
     
     message_title = "Hello World"
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # DistSys-Push-Architecture
 
 This is a Push architecture written in Python with a Google Firebase library pyfcm
+
+[Documentation](https://docs.google.com/document/d/1voiZqLEOYJ7eHDC9GasU-48b6zf34G8UaqzCGqaJ20I/edit?usp=sharing)
