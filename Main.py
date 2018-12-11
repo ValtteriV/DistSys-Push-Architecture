ba5e1f4d94036141e0701b520d30c2c9bd5ba1d1
Valtteri Vistiaho <valtteri.vistiaho@gmail.com>
2018-12-11 18:18:35 +0200

    Basic text interface for testing

--- Main.py
+++ Main.py
@@ -6,12 +6,17 @@ if __name__ == '__main__':
     
     message_title = "Hello World"
     
-    message_body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis."
+    message_body_short = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis."
+    message_body_average = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis."
+    message_body_long = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis."
     
+    choice = input("Choose the test type with a number of 1-4: \n 1: Send 50 random messages. \n 2: Send 25 of each message type \n 3: Measure the delay between receiving average messages. \n 4: Stress test")
+
+
     
     result = push_service.notify_multiple_devices(registration_ids=registration_ids,
                                          message_title=message_title,
-                                         message_body=message_body)
+                                         message_body=message_body_short)
     
     print(result)
     
