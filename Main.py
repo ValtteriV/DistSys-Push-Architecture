6c55e58bb8ea936ef2bfdc7c2789017d9375da09
Olli-Pekka Lindstrom <olli-pekka.lindstrom@helsinki.fi>
2018-12-11 18:47:24 +0200

    reg ids

--- Main.py
+++ Main.py
@@ -3,7 +3,11 @@ import random
 
 
 push_service = FCMNotification(api_key="AAAA8un-MkA:APA91bFJnBY7q37ERY3cq4F5QULtsV_-YuWLG1e_CxhSfRKzeusdlx-PW73cDdHCzNign3YZvdFv0yeg0O6UikwdVCgo1cmOamrJRqXWHBG19WPJfNS3dXg4fRyGQMa66c5fF3MYOoqG") #api_key from firebase project
-registration_ids = [None] #The id of the client(s) to receive messages
+
+file = open('regids.txt','r') 
+regid = file.read()
+
+registration_ids = [regid] #The id of the client(s) to receive messages
     
 message_title = "Hello World"
     
@@ -45,13 +49,13 @@ if __name__ == '__main__':
     
     choice = input("Choose the test type with a number of 1-4: \n 1: Send 50 random messages. \n 2: Send 25 of each message type \n 3: Measure the delay between receiving average messages. \n 4: Stress test \n")
 
-    if choice == 1:
+    if choice == "1":
         random_messages()
-    elif choice == 2:
+    elif choice == "2":
         variety_messages()
-    elif choice == 3:
+    elif choice == "3":
         consistent_average()
-    elif choice == 4:
+    elif choice == "4":
         stress_test()
     else: 
         print("Input error")
